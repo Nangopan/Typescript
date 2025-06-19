2d411231dfca13f11d78739bc65351eb57fdda5a
Nangopa <nangopan888@gmail.com>
2025-06-19 11:15:54 +0530

    Basics

---
+++ index.js
@@ -0,0 +1,6 @@
+// string
+var strVal = "hello world";
+// number
+var Id = 12345;
+// boolean
+var isExists = true;
---
+++ index.ts
@@ -0,0 +1,9 @@
+
+// string
+let strVal:string="hello world"
+
+// number
+let Id:number=12345
+
+// boolean
+let isExists:boolean=true
